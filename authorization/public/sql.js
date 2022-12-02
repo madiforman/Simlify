ea65420531a70d37d1c6282f4a1fcb7d94308cb1
Ben McAuliffe <benmcauliffe@Bens-MacBook-Pro.local>
2022-12-01 17:25:12 -0800

    added external db

--- authorization/public/sql.js
+++ authorization/public/sql.js
@@ -1,36 +1,80 @@
 var initSqlJs = require("./sql-wasm.js");
-/*Thinking we have the user download the two DB files and then supply them 
-*/
+//import initSqlJs from "./sql-wasm.js";
+/*Thinking we have the user download the two DB files and then supply them
+ */
 
 initSqlJs().then(function (SQL) {
   console.log("sql.js initialized ");
-  const db = new SQL.Database()
-  db.run("CREATE TABLE Music (trackID, trackName, Acousticness, Danceability, Energy, Liveness, Valence, Speechiness, Tempo)");
+  const db = new SQL.Database();
+  db.run(
+    "CREATE TABLE Music (trackID, trackName, Acousticness, Danceability, Energy, Liveness, Valence, Speechiness, Tempo)"
+  );
   db.run(
     `INSERT INTO topTrackWeek (id, songName, artistName, genre, streams)
-        VALUES (1, 'sad song', 'sad guy', 'Acoustic', 200)`);
-  db.run("CREATE TABLE otherUserTopTrackWeek (id, songName, artistName, genre, streams);");
+        VALUES (1, 'sad song', 'sad guy', 'Acoustic', 200)`
+  );
+  db.run(
+    "CREATE TABLE otherUserTopTrackWeek (id, songName, artistName, genre, streams);"
+  );
   db.run(
     `INSERT INTO otherUserTopTrackWeek (id, songName, artistName, genre, streams)
-        VALUES (2, 'happy song', 'happy guy', 'Acoustic', 200)`)				
-  const db2 = new SQL.Database()
-  db2.run("CREATE TABLE topTrackWeek (id, songName, artistName, genre, streams);");
+        VALUES (2, 'happy song', 'happy guy', 'Acoustic', 200)`
+  );
+  const db2 = new SQL.Database();
+  db2.run(
+    "CREATE TABLE topTrackWeek (id, songName, artistName, genre, streams);"
+  );
   db2.run(
     `INSERT INTO topTrackWeek (id, songName, artistName, genre, streams)
-        VALUES (1, 'happy song', 'happy guy', 'Acoustic', 200)`)		
-  var stmt = db.prepare("SELECT * FROM topTrackWeek NATURAL JOIN otherUserTopTrackWeek WHERE id BETWEEN $start AND $end");
+        VALUES (1, 'happy song', 'happy guy', 'Acoustic', 200)`
+  );
+  var stmt = db.prepare(
+    "SELECT * FROM topTrackWeek NATURAL JOIN otherUserTopTrackWeek WHERE id BETWEEN $start AND $end"
+  );
   stmt.bind({ $start: 1, $end: 2 });
   while (stmt.step()) {
     var row = stmt.getAsObject();
     console.log("Here is a sample row from DB1: " + JSON.stringify(row));
   }
-  var stmt2 = db2.prepare("SELECT * FROM topTrackWeek WHERE id BETWEEN $start AND $end");
+  var stmt2 = db2.prepare(
+    "SELECT * FROM topTrackWeek WHERE id BETWEEN $start AND $end"
+  );
   stmt2.bind({ $start: 1, $end: 2 });
   while (stmt2.step()) {
     var row = stmt2.getAsObject();
     console.log("Here is a sample row from DB2: " + JSON.stringify(row));
   }
 
+  // ben
 
+  let acoutsticnessOne = [];
+  let acoutsticnessTwo = [];
 
+  db.all(
+    "SELECT Acousticness FROM Music * Users&Music WHERE id == 1",
+    [],
+    (err, rows) => {
+      if (err) {
+        return console.error(err.message);
+      }
+      rows.forEach((row) => {
+        acoutsticnessOne.push(row);
+      });
+    }
+  );
+
+  db.all(
+    "SELECT Acousticness FROM Music * Users&Music WHERE id == 2",
+    [],
+    (err, rows) => {
+      if (err) {
+        return console.error(err.message);
+      }
+      rows.forEach((row) => {
+        acoutsticnessTwo.push(row);
+      });
+    }
+  );
+
+  //find similarity
 });
--- authorization/script.js
+++ authorization/script.js
@@ -6,25 +6,27 @@
  * For more information, read
  * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
  */
-var express = require('express'); // Express web server framework
-var request = require('request'); // "Request" library
-var cors = require('cors');
-var querystring = require('querystring');
-var cookieParser = require('cookie-parser');
-var sqlite3 = require('sqlite3').verbose();
+var express = require("express"); // Express web server framework
+var request = require("request"); // "Request" library
+var cors = require("cors");
+var querystring = require("querystring");
+var cookieParser = require("cookie-parser");
+var sqlite3 = require("sqlite3").verbose();
+var db = new sqlite3.Database("455app.db");
 
-var client_id = 'a2bd214fd5d44b278a1625e0f5376057'; // Your client id
-var client_secret = '546e170d6d1042eeab670d4f84d233f8'; // Your secret
-var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+var client_id = "a2bd214fd5d44b278a1625e0f5376057"; // Your client id
+var client_secret = "546e170d6d1042eeab670d4f84d233f8"; // Your secret
+var redirect_uri = "http://localhost:8888/callback"; // Your redirect uri
 
 /**
-* Generates a random string containing numbers and letters
-* @param  {number} length The length of the string
-* @return {string} The generated string
-*/
-var generateRandomString = function(length) {
-  var text = '';
-  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+var generateRandomString = function (length) {
+  var text = "";
+  var possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
   for (var i = 0; i < length; i++) {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
@@ -32,33 +34,35 @@ var generateRandomString = function(length) {
   return text;
 };
 
-var stateKey = 'spotify_auth_state';
+var stateKey = "spotify_auth_state";
 
 var app = express();
 
-app.use(express.static(__dirname + '/public'))
+app
+  .use(express.static(__dirname + "/public"))
   .use(cors())
   .use(cookieParser());
 
-app.get('/login', function(req, res) {
-
+app.get("/login", function (req, res) {
   var state = generateRandomString(16);
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope = 'user-read-private user-read-email user-top-read';
-  res.redirect('https://accounts.spotify.com/authorize?' +
-    querystring.stringify({
-      response_type: 'code',
-      client_id: client_id,
-      scope: scope,
-      redirect_uri: redirect_uri,
-      state: state,
-      show_dialog: true,
-    }));
+  var scope = "user-read-private user-read-email user-top-read";
+  res.redirect(
+    "https://accounts.spotify.com/authorize?" +
+      querystring.stringify({
+        response_type: "code",
+        client_id: client_id,
+        scope: scope,
+        redirect_uri: redirect_uri,
+        state: state,
+        show_dialog: true,
+      })
+  );
 });
 
-app.get('/callback', function(req, res) {
+app.get("/callback", function (req, res) {
   // your application requests refresh and access tokens
   // after checking the state parameter
 
@@ -67,48 +71,54 @@ app.get('/callback', function(req, res) {
   var storedState = req.cookies ? req.cookies[stateKey] : null;
 
   if (state === null || state !== storedState) {
-    res.redirect('/#' +
-      querystring.stringify({
-        error: 'state_mismatch'
-      }));
+    res.redirect(
+      "/#" +
+        querystring.stringify({
+          error: "state_mismatch",
+        })
+    );
   } else {
     res.clearCookie(stateKey);
     var authOptions = {
-      url: 'https://accounts.spotify.com/api/token',
+      url: "https://accounts.spotify.com/api/token",
       form: {
         code: code,
         redirect_uri: redirect_uri,
-        grant_type: 'authorization_code'
+        grant_type: "authorization_code",
       },
       headers: {
-        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+        Authorization:
+          "Basic " +
+          new Buffer(client_id + ":" + client_secret).toString("base64"),
       },
-      json: true
+      json: true,
     };
 
-    request.post(authOptions, function(error, response, body) {
+    request.post(authOptions, function (error, response, body) {
       if (!error && response.statusCode === 200) {
         access_token = body.access_token;
         var access_token = body.access_token,
           refresh_token = body.refresh_token;
- 
+
         //  var options = {
         //    url: 'https://api.spotify.com/v1/me',
         //    headers: { 'Authorization': 'Bearer ' + access_token },
         //    json: true
         //  };
- 
+
         //  // use the access token to access the Spotify Web API
         //  request.get(options, function(error, response, body) {
         //    console.log(body);
         //  });
- 
-         // we can also pass the token to the browser to make requests from there
-        res.redirect('/#' +
-          querystring.stringify({
-            access_token: access_token,
-            refresh_token: refresh_token
-          }));
+
+        // we can also pass the token to the browser to make requests from there
+        res.redirect(
+          "/#" +
+            querystring.stringify({
+              access_token: access_token,
+              refresh_token: refresh_token,
+            })
+        );
       } else {
         //  res.redirect('/#' +
         //    querystring.stringify({
@@ -119,31 +129,33 @@ app.get('/callback', function(req, res) {
     });
   }
 });
- 
-app.get('/refresh_token', function(req, res) {
 
+app.get("/refresh_token", function (req, res) {
   // requesting access token from refresh token
   var refresh_token = req.query.refresh_token;
   var authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
-    form: {
-      grant_type: 'refresh_token',
-      refresh_token: refresh_token
+    url: "https://accounts.spotify.com/api/token",
+    headers: {
+      Authorization:
+        "Basic " +
+        new Buffer(client_id + ":" + client_secret).toString("base64"),
     },
-    json: true
+    form: {
+      grant_type: "refresh_token",
+      refresh_token: refresh_token,
+    },
+    json: true,
   };
 
-  request.post(authOptions, function(error, response, body) {
+  request.post(authOptions, function (error, response, body) {
     if (!error && response.statusCode === 200) {
       var access_token = body.access_token;
       res.send({
-        'access_token': access_token
+        access_token: access_token,
       });
     }
   });
 });
 
-console.log('Listening on 8888');
+console.log("Listening on 8888");
 app.listen(8888);
-
